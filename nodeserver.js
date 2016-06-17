a73324f72672ecfaf10878cd5141dd825e02a2c9
ansroman <anssi.roman@gmail.com>
2016-06-17 13:24:27 +0300

    AWS side nodejs server

---
+++ nodeserver.js
@@ -0,0 +1,25 @@
+"use strict";
+var restify = require("restify");
+var server = restify.createServer({
+  name: 'VattuMittari',
+  version: '1.0.0'
+});
+
+// REST API implementation for handling the push messages from the Raspberry Pi
+server.post('/sendMeasurements', function (req, res, next) {
+    var time = new Date();
+    var hh = addZero(time.getHours());
+    var mm = addZero(time.getMinutes());
+    var ss = addZero(time.getSeconds());
+    var consoleTime = hh + ":" + mm + ":" + ss; 
+    
+    console.log('got temperature reading from RasPi. Timestamp ' + consoleTime); // remove this
+    handleSenses(req.params[0].senses, time);
+
+    res.send(Number(200)); // send reply
+    next();
+});
+
+server.listen(8080, function () {
+    console.log('Node.js RasPi %s', server.url);
+});
